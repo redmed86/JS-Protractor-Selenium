eef9854e7c6572ddcadb0113b5bf28b21c1e2882
Derek Ross <derekross@Derek-Ross-SL-0425.local>
2018-03-13 13:54:56 -0700

    removing unnecessary files and ignoring lock files

--- conf.js
+++ conf.js
@@ -25,9 +25,37 @@ exports.config = {
         name: "firefox-tests",
         shardTestFiles: true,
         maxInstances: 25
+    },{
+        browserName: 'firefox',
+        version: 'latest',
+        platform: 'Windows 7',
+        name: "firefox-tests",
+        shardTestFiles: true,
+        maxInstances: 25
+    }, {
+        browserName: 'firefox',
+        version: 'latest',
+        platform: 'Windows 10',
+        name: "firefox-tests",
+        shardTestFiles: true,
+        maxInstances: 25
     }, {
         browserName: 'chrome',
         version: '41',
+        platform: 'Windows 7',
+        name: "chrome-tests",
+        shardTestFiles: true,
+        maxInstances: 25
+    }, {
+        browserName: 'chrome',
+        version: '41',
+        platform: 'OS X 10.10',
+        name: "chrome-tests",
+        shardTestFiles: true,
+        maxInstances: 25
+    }, {
+        browserName: 'chrome',
+        version: 'latest',
         platform: 'Windows 7',
         name: "chrome-tests",
         shardTestFiles: true,
@@ -41,6 +69,6 @@ exports.config = {
                 console.log('SauceOnDemandSessionID=' + session.getId() + ' job-name=' + jobName);
             });
         }
-        printSessionId("Insert Job Name Here");
+        printSessionId("Derek's Protractor Tests");
     }
 }
