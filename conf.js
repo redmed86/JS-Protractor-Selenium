7327b0b1dae136dfa7d6d5a8292de213f9716e7e
Derek Ross <derekross@Derek-Ross-SL-0425.local>
2018-03-13 22:28:16 -0700

    trying sauceBuild to update build tag in Sauce Labs

--- conf.js
+++ conf.js
@@ -5,7 +5,7 @@
 exports.config = {
     sauceUser: process.env.SAUCE_USERNAME,
     sauceKey: process.env.SAUCE_ACCESS_KEY,
-
+    sauceBuild: "Derek Jenkins: " + process.env.BUILD_NUMBER,
     // sauceAgent: sauceRestAgent,
 
     // webDriverProxy: 'http://<proxy>:<port>',
@@ -17,8 +17,7 @@ exports.config = {
 
     onPrepare: function () {
         var caps = browser.getCapabilities()
-        // "Derek - SE Jenkins" = "Derek Jenkins"
-        // console.log(caps)
+        console.log(caps)
     },
 
     multiCapabilities: [{
@@ -27,48 +26,42 @@ exports.config = {
         platform: 'OS X 10.10',
         name: "firefox-tests",
         shardTestFiles: true,
-        maxInstances: 25,
-        build: "Derek - SE Jenkins"
+        maxInstances: 25
     },{
         browserName: 'firefox',
         version: 'latest',
         platform: 'Windows 7',
         name: "firefox-tests",
         shardTestFiles: true,
-        maxInstances: 25,
-        build: "Derek - SE Jenkins"
+        maxInstances: 25
     }, {
         browserName: 'firefox',
         version: 'latest',
         platform: 'Windows 10',
         name: "firefox-tests",
         shardTestFiles: true,
-        maxInstances: 25,
-        build: "Derek - SE Jenkins"
+        maxInstances: 25
     }, {
         browserName: 'chrome',
         version: '41',
         platform: 'Windows 7',
         name: "chrome-tests",
         shardTestFiles: true,
-        maxInstances: 25,
-        build: "Derek - SE Jenkins"
+        maxInstances: 25
     }, {
         browserName: 'chrome',
         version: '41',
         platform: 'OS X 10.10',
         name: "chrome-tests",
         shardTestFiles: true,
-        maxInstances: 25,
-        build: "Derek - SE Jenkins"
+        maxInstances: 25
     }, {
         browserName: 'chrome',
         version: 'latest',
         platform: 'Windows 7',
         name: "chrome-tests",
         shardTestFiles: true,
-        maxInstances: 25,
-        build: "Derek - SE Jenkins"
+        maxInstances: 25
     }],
 
     onComplete: function () {
